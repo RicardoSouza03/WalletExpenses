9a00202d75e3dae6b331a0fa5e132b628faae17f
Ricardo_Souza <rl03souza@gmail.com>
2022-08-30 15:28:23 -0300

    adicionando requisito 8/ alimentando a tabela com as despesas adicionadas

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const expensesCards = expenses && expenses.map((expense) => {
+      const { value, description, currency, method, tag, exchangeRates, id } = expense;
+      return (
+        <tbody key={ id }>
+          <tr>
+            <td>{ description }</td>
+            <td>{ tag }</td>
+            <td>{ method }</td>
+            <td>{ parseFloat(value).toFixed(2) }</td>
+            <td>{ exchangeRates[currency].name }</td>
+            <td>{ parseFloat(exchangeRates[currency].ask).toFixed(2) }</td>
+            <td>{ parseFloat(value * exchangeRates[currency].ask).toFixed(2) }</td>
+            <td>Real</td>
+            <td>apagar/editar</td>
+          </tr>
+        </tbody>
+      );
+    });
+
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <caption>Despesas gastas</caption>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        { expensesCards }
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet: { expenses } }) => ({
+  expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Table);
